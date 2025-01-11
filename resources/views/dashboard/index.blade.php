b95602211ec45654c07b21862e4e2c795e5fa106
GabrielHenriqueM <gabrielmoreiraptc9@gmail.com>
2025-01-11 13:02:05 -0300

    Adicionado arquivo dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="bg-light shadow p-4 rounded text-center">
+            <h1>Bem-vindo ao Sistema de Gestão</h1>
+            <p class="lead mt-3">Use o menu lateral para acessar todas as funcionalidades!</p>
+        </div>
+    </div>
+@endsection
